6c3117227a2ab9f07efdfc9f11dcdd0a7a5cfb88
sainathh <51146038+sainathh@users.noreply.github.com>
2020-10-27 16:55:41 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	ini_set('display_errors',1);
 	error_reporting(-1);
 	define('DB_HOST', 'localhost');
-    define('DB_USER', $dbuser);
-    define('DB_PASSWORD', $dbpasswd);
-    define('DB_DATABASE', $dbname);
+    define('DB_USER', '$dbuser');
+    define('DB_PASSWORD', '$dbpasswd');
+    define('DB_DATABASE', '$dbname');
 ?>
